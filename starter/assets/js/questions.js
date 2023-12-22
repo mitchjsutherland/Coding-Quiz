0b0415214da0a6efa2a38f658beab39cb40b6b3a
Mitch Sutherland <mitchelsutherland@icloud.com>
2023-12-22 16:35:07 +0000

    Pseudo

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,21 @@
+// start button to initiate timer + first question appear
+
+
+
+// questions contain 4 answer buttons 
+
+
+
+
+// answer button clicks to next question
+// if incorrect answer subtract time
+
+
+
+
+// quiz end at question.length end or timer === 0
+
+
+
+
+// quiz end displays score -> score.js file for score page logic
